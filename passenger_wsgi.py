511da47092034a276a8f2663bc80d70d05c21914
Emmanuel <nwekeemmanuel667@gmail.com>
2025-09-03 16:42:08 +0100

    Push project to cPanel

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,12 @@
-import sys
-import os
+import sys, os, traceback
 
-# Add your project directory to the Python path
-sys.path.insert(0, '/home/waapfoli/waapfolio')
+try:
+    sys.path.insert(0, '/home/waapfoli/waapfolio')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
-# Set the Django settings module
-os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
-
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+except Exception:
+    with open("/home/waapfoli/waapfolio/wsgi_debug.log", "w") as f:
+        f.write(traceback.format_exc())
+    raise
