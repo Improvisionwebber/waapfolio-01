98bb598c7948a88cb1f6ca863ccd50aac5ac6a18
Emmanuel <nwekeemmanuel667@gmail.com>
2025-09-03 17:11:52 +0100

    Push project to cPanel

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,20 @@
-import sys, os, traceback
+import sys
+import os
+import traceback
 
 try:
-    sys.path.insert(0, '/home/waapfoli/waapfolio')
+    # Add the public_html folder (app root)
+    sys.path.insert(0, '/home/waapfoli/public_html')
+    sys.path.insert(1, '/home/waapfoli/public_html/project')  # Django settings folder
+
+    # Set Django settings
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
+    # Get WSGI application
     from django.core.wsgi import get_wsgi_application
     application = get_wsgi_application()
+
 except Exception:
-    with open("/home/waapfoli/waapfolio/wsgi_debug.log", "w") as f:
+    with open("/home/waapfoli/public_html/wsgi_debug.log", "w") as f:
         f.write(traceback.format_exc())
     raise
--- project/wsgi.py
+++ project/wsgi.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-# Add your project directory to the Python path
-sys.path.insert(0, '/home/waapfoli/waapfolio')  # <- your cPanel project path
-sys.path.insert(1, '/home/waapfoli/waapfolio/project')  # optional if Django settings are in 'project'
+# Add your public_html folder (app root)
+sys.path.insert(0, '/home/waapfoli/public_html')
+sys.path.insert(1, '/home/waapfoli/public_html/project')  # Django settings folder
 
-from django.core.wsgi import get_wsgi_application
-
+# Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
