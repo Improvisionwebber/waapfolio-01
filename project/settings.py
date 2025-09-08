8bf6e9b81c822ce5de7b7e87a5017e8bda6f6e30
Emmanuel <nwekeemmanuel667@gmail.com>
2025-09-08 23:07:16 +0100

    fixed price in product detail

--- project/settings.py
+++ project/settings.py
@@ -64,7 +64,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
-# ✅ MySQL Database (from .env)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -74,11 +73,12 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '3306'),
         'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4',
+            'init_command': "SET NAMES 'utf8mb4' COLLATE 'utf8mb4_general_ci', sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
+
 
 
 # Password validation
